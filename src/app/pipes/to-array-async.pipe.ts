092254af9f78d0909027d1d41f20f7dcb12b9cb7
Ryszard Brzegowy <rbrzegowy@wsei.edu.pl>
2024-11-24 15:27:36 +0100

    rxjs3, toArray pipe

--- src/app/pipes/to-array-async.pipe.ts
+++ src/app/pipes/to-array-async.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core'
-import { Observable, scan } from 'rxjs'
+import { map, Observable, scan } from 'rxjs'
 
 @Pipe({
   name: 'toArray',
@@ -7,9 +7,19 @@ import { Observable, scan } from 'rxjs'
 })
 export class ToArrayPipe implements PipeTransform {
 
-  transform(value: Observable<unknown>): Observable<unknown> {
-    const accumulated = value.pipe(scan((acc, curr) => [...acc, curr], [] as unknown[]))
+  transform(value: Observable<unknown>, showOnlyLatest = 0): Observable<unknown> {
+    const accumulated = value.pipe(
+      scan((acc, curr) => [...acc, curr], [] as unknown[]),
+      map(arr => {
+        if (showOnlyLatest === 0 || arr.length <= showOnlyLatest) {
+          return arr
+        } else {
+          const onlyLatest = arr.slice(-showOnlyLatest)
+          const hiddenCount = arr.length - showOnlyLatest
+          onlyLatest.unshift(`[+${hiddenCount} wcześniej]`)
+          return onlyLatest
+        }
+      }))
     return accumulated
   }
-
 }
